2471b6bfb32ae9ca9253a4f2b179728a7d99246f
benhenshaw <benedicthenshaw@gmail.com>
2018-11-26 21:26:33 +0000

    Added error reporting for web server

--- server.go
+++ server.go
@@ -8,10 +8,9 @@ import (
 func main() {
     static_dir := "web/"
     address := ":80"
-
     fs := http.FileServer(http.Dir("web/"))
     http.Handle("/", fs)
-
     fmt.Printf("Launching server on %s, serving files from '%s'.\n", address, static_dir)
-    http.ListenAndServe(address, nil)
+    e := http.ListenAndServe(address, nil)
+    fmt.Printf("Server exited (%s)", e);
 }
