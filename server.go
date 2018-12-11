1662af1452a4d43971642f647bce61a892e0f308
benhenshaw <benedicthenshaw@gmail.com>
2018-12-11 14:38:19 +0000

    Added fall back to http in absence of TLS cert/key

--- server.go
+++ server.go
@@ -8,11 +8,12 @@ import (
 )
 
 func main() {
-    cert := flag.String("cert", "", "TLS certificate.")
-    key  := flag.String("key",  "", "TLS private key.")
-    flag.Parse();
+    cert       := flag.String("cert", "",     "TLS certificate.")
+    key        := flag.String("key",  "",     "TLS private key.")
+    static_dir := flag.String("path", "web/", "Static file directory.")
+    flag.Parse()
 
-    var upgrader = websocket.Upgrader {
+    var upgrader = websocket.Upgrader{
         ReadBufferSize:  1024,
         WriteBufferSize: 1024,
     }
@@ -42,29 +43,47 @@ func main() {
 
             for _, c := range connections {
                 e = c.WriteMessage(message_type, message)
-                if (e != nil) {
+                if e != nil {
                     fmt.Printf("ERROR (WriteMessage): %s", e)
                 }
             }
         }
     })
 
-    address := ":443"
-    static_dir := "web/"
-    fs := http.FileServer(http.Dir(static_dir))
-    http.Handle("/", fs)
-    fmt.Printf("Launching server on %s, serving files from '%s'.\n", address, static_dir)
-    http.ListenAndServeTLS(address, *cert, *key, nil)
+    if *cert != "" && *key != "" {
+        file_server := http.FileServer(http.Dir(*static_dir))
+        http.Handle("/", file_server)
 
-    // Forward http requests to https.
+        address := ":443"
+        fmt.Printf("Launching HTTPS server on %s, serving files from '%s'.\n", address, *static_dir)
+        e := http.ListenAndServeTLS(address, *cert, *key, nil)
+        if e != nil {
+            fmt.Printf("HTTPS Server Error:\n%s\n", e)
+        }
 
-    redirector := http.HandlerFunc(func (writer http.ResponseWriter, request *http.Request) {
-        http.Redirect(
-            writer,
-            request,
-            "https://" + request.Host + request.RequestURI,
-            http.StatusMovedPermanently)
-    });
-    http.ListenAndServe(":80", redirector);
+        // Forward http requests to https.
+        redirector := http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+            http.Redirect(
+                writer,
+                request,
+                "https://"+request.Host+request.RequestURI,
+                http.StatusMovedPermanently)
+        })
+        redirect_address := ":80"
+        fmt.Printf("Launched HTTP -> HTTPS redirect server on %s.\n", redirect_address)
+        e = http.ListenAndServe(redirect_address, redirector)
+        if e != nil {
+            fmt.Printf("HTTP Server Error:\n%s\n", e)
+        }
+    } else {
+        file_server := http.FileServer(http.Dir(*static_dir))
+        http.Handle("/", file_server)
 
+        address := ":80"
+        fmt.Printf("Launching HTTP server on %s, serving files from '%s'.\n", address, *static_dir)
+        e := http.ListenAndServe(address, nil)
+        if e != nil {
+            fmt.Printf("HTTP Server Error:\n%s\n", e)
+        }
+    }
 }
